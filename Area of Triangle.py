a86627ac42a9b63eb97a100127814e31e81ee257
Meetkumar Patel <86229515+meetpatel1111@users.noreply.github.com>
2022-02-03 21:27:15 +0530

    Add files via upload
    first

---
+++ Area of Triangle.py
@@ -0,0 +1,12 @@
+# Python program to calculate area of triangle
+a = 3
+b = 4
+c = 5
+
+#a = float(input("Enter first side: "))
+#b = float(input("Enter second side: "))
+#c = float(input("Enter third side: "))
+
+s = (a + b + c)/2
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+print("The area of the triangle is %0.2f"%area)
